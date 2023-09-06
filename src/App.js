e84edad94552d3b7b66e15e9a4454b00cd093ea8
payal033 <82137135+payal033@users.noreply.github.com>
2023-09-06 23:24:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Concepts from "./Concepts";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Concepts />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import keyConceptsImage from "./assets/images/key-concepts.png";
+export default function Headers() {
+  return (
+    <header>
+      <img src={keyConceptsImage} alt="Medal badge with a star" />
+      <h1>Key React Concepts</h1>
+      <p>Selected key React concepts you should know about</p>
+    </header>
+  );
+}
